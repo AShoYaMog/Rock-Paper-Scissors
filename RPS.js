ba6fa2232a59771c78c8a60114690f6a79d3cae1
AShoYaMog <ivan.litvinenko.personal@gmail.com>
2022-07-26 00:27:32 +0300

    finish game logik

--- RPS.js
+++ RPS.js
@@ -1,3 +1,4 @@
+let playersScore = [0,0];
 
 
 
@@ -12,19 +13,51 @@ function computerSelection() {
 }
 
 function playRound(player,computer) {
-    player = playerSelection();
-    computer = computerSelection();
     if (computer == player) {
-        alert('Round')
+        alert('Round')        
+        return 'Round';
     } else if (
         (computer == 'rock' && player == 'scissors')||
         (computer == 'scissors' && player == 'paper')||
         (computer == 'paper' && player == 'rock')) {
-            alert('Computer Win')
+        alert('Computer Win')
+        return 'Computer Win';
     } else {
-        alert('player win')
+        alert('player win') 
+        return 'player win';
     }
 }
 
+function score(playRound) {   
+    if (playRound == 'Computer Win') {
+       return playersScore[1] = ++playersScore[1];
+    } else if (playRound == 'player win') {
+       return playersScore[0] = ++playersScore[0];
+    } else {
+        console.log('blet');
+    }
+}
 
-playRound()
+function gameResult() {
+    if (playersScore[0] > playersScore[1]) {
+        alert('Game result \nPlayer win');
+    } else if (playersScore[0] < playersScore[1]) {
+        alert('Game result: \nComputer win');
+    } else {
+        alert('Game result: \nDead heat')
+    }
+} 
+
+function game() {
+    let i = 0
+    while ((i < 5)&&((playersScore[0]||playersScore[1])<3)) {
+        score(playRound(playerSelection(),computerSelection()));
+        alert(`Round: ${(i+1)}\nPlayer score: ${playersScore[0]}\nComputer score: ${playersScore[1]}`);
+        i++;
+    }
+    gameResult();
+    playersScore = [0,0];
+}
+
+
+game()
